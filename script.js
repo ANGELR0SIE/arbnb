3f8c5667f116dd142989288d3a8a0618daef4876
angelrosie <angelpulikkayath@gmail.com>
2024-07-17 16:13:42 +0530

    jul-17

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ window.onscroll = function() {
     const searchBar = document.querySelector('.search-bar-hidden');
     const navBar = document.querySelector('.nav-bar-743px');
     const staysInsideNav = document.getElementById('stays-inside-nav');
+    const category = document.querySelector('categories');
+    // const spacer = document.querySelector('.flex-spacer-nav-bar-743px');
   
     if (categories && searchBar && navBar && staysInsideNav) {
       if (window.scrollY >= navBar.offsetTop) {
@@ -14,11 +16,15 @@ window.onscroll = function() {
         if (window.scrollY >= categories.offsetTop - navBar.offsetHeight) {
           searchBar.style.display = 'flex';
           staysInsideNav.style.display = 'none';
+          // spacer.style.display = 'none';
+          category.classList.add('sticky');
         } else {
           searchBar.style.display = 'none';
-          staysInsideNav.style.display = 'block';
+          staysInsideNav.style.display = 'flex';
+          
         }
-      } else {
+      } 
+      else {
         navBar.classList.remove('fixed');
         searchBar.style.display = 'none';
         staysInsideNav.style.display = 'block';
