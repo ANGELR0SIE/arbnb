5dbaefd3d143cbc3d2ae0b4bdb988fcc164c984a
angelrosie <angelpulikkayath@gmail.com>
2024-07-18 10:11:06 +0530

    jul-18

--- script.js
+++ script.js
@@ -45,39 +45,108 @@
 //         stickyDiv.style.position = 'relative';
 //     }
 // });
-window.onscroll = function() {
-  toggleElementsOnScroll();
-};
+// window.onscroll = function() {
+//   toggleElementsOnScroll();
+// };
 
+// function toggleElementsOnScroll() {
+//   const categories = document.querySelector('.category-flex');
+//   const searchBar = document.querySelector('.search-bar-hidden');
+//   const navBar = document.querySelector('.nav-bar-743px');
+//   const staysInsideNav = document.getElementById('stays-inside-nav');
+
+//   if (categories && searchBar && navBar && staysInsideNav) {
+//       if (window.scrollY > navBar.offsetTop) {
+//           navBar.classList.add('fixed');
+//           if (window.scrollY >= categories.offsetTop - navBar.offsetHeight &&m) {
+//               searchBar.style.display = 'flex';
+//               staysInsideNav.style.display = 'none';
+//               categories.style.position = 'fixed';
+//               categories.style.top = '48px';
+//               categories.style.right = '0';
+//               categories.style.left = '0';
+//           } else {
+//               searchBar.style.display = 'none';
+//               // staysInsideNav.style.display = 'block';
+//               categories.style.position = 'static';
+//               categories.forEach(category => {
+//                 category.style.borderBottom = '2px solid rgba(0, 0, 0, 0.08)';
+//             });
+//           }
+//       } else {
+//           navBar.classList.remove('fixed');
+//           categories.style.position = 'static';
+//           searchBar.style.display = 'none';
+//           staysInsideNav.style.display = 'flex';
+//       }
+//   }
+// }
 function toggleElementsOnScroll() {
-  const categories = document.querySelector('.category-flex');
-  const searchBar = document.querySelector('.search-bar-hidden');
-  const navBar = document.querySelector('.nav-bar-743px');
-  const staysInsideNav = document.getElementById('stays-inside-nav');
+  if (window.innerWidth >= 744) {
+    const categories = document.querySelector('.category-flex');
+    const searchBar = document.querySelector('.search-bar-hidden');
+    const navBar = document.querySelector('.nav-bar-743px');
+    const staysInsideNav = document.getElementById('stays-inside-nav');
 
-  if (categories && searchBar && navBar && staysInsideNav) {
-      if (window.scrollY > navBar.offsetTop) {
-          navBar.classList.add('fixed');
-          if (window.scrollY >= categories.offsetTop - navBar.offsetHeight) {
-              searchBar.style.display = 'flex';
-              staysInsideNav.style.display = 'none';
-              categories.style.position = 'fixed';
-              categories.style.top = '48px';
-              categories.style.right = '0';
-              categories.style.left = '0';
-          } else {
-              searchBar.style.display = 'none';
-              // staysInsideNav.style.display = 'block';
-              categories.style.position = 'static';
-              categories.forEach(category => {
-                category.style.borderBottom = '2px solid rgba(0, 0, 0, 0.08)';
-            });
-          }
-      } else {
-          navBar.classList.remove('fixed');
-          categories.style.position = 'static';
-          searchBar.style.display = 'none';
-          staysInsideNav.style.display = 'flex';
-      }
-  }
+    if (categories && searchBar && navBar && staysInsideNav) {
+        if (window.scrollY > navBar.offsetTop) {
+            navBar.classList.add('fixed');
+            if (window.scrollY >= categories.offsetTop - navBar.offsetHeight) {
+                searchBar.style.display = 'flex';
+                staysInsideNav.style.display = 'none';
+                categories.style.position = 'fixed';
+                categories.style.top = '48px';
+                categories.style.right = '0';
+                categories.style.left = '0';
+                categories.style.borderTop = 'none';
+            } else {
+                searchBar.style.display = 'none';
+                staysInsideNav.style.display = 'none';
+                categories.style.position = 'static';
+                categories.style.borderBottom = '2px solid rgba(0, 0, 0, 0.08)';
+                
+            }
+        } else {
+            navBar.classList.remove('fixed');
+            categories.style.position = 'static';
+            searchBar.style.display = 'none';
+            staysInsideNav.style.display = 'none';
+            categories.style.borderTop =  '1px solid rgba(0, 0, 0, 0.08)';
+            categories.style.borderBottom = 'none';
+        }
+    }
 }
+    if (window.innerWidth >= 950) {
+        const categories = document.querySelector('.category-flex');
+        const searchBar = document.querySelector('.search-bar-hidden');
+        const navBar = document.querySelector('.nav-bar-743px');
+        const staysInsideNav = document.getElementById('stays-inside-nav');
+
+        if (categories && searchBar && navBar && staysInsideNav) {
+            if (window.scrollY > navBar.offsetTop) {
+                navBar.classList.add('fixed');
+                if (window.scrollY >= categories.offsetTop - navBar.offsetHeight) {
+                    searchBar.style.display = 'flex';
+                    staysInsideNav.style.display = 'none';
+                    categories.style.position = 'fixed';
+                    categories.style.top = '48px';
+                    categories.style.right = '0';
+                    categories.style.left = '0';
+                } else {
+                    searchBar.style.display = 'none';
+                    staysInsideNav.style.display = 'flex';
+                    categories.style.position = 'static';
+                    categories.style.borderBottom = '2px solid rgba(0, 0, 0, 0.08)';
+                }
+            } else {
+                navBar.classList.remove('fixed');
+                categories.style.position = 'static';
+                searchBar.style.display = 'none';
+                staysInsideNav.style.display = 'flex';
+            }
+        }
+    }
+    
+}
+
+window.addEventListener('scroll', toggleElementsOnScroll);
